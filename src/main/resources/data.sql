3476bfa0b046c8198f9c9d963736699bdaabb91e
debmeetDev <debmeet.banerjee@gmail.com>
2023-02-26 13:19:58 +0530

    .

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+select * from COURSE;
+
+insert into COURSE(ID, AUTHOR, COURSE_NAME)
+values(100001, 'debmeetPublication', 'learn abcde');
+
+insert into COURSE(ID, AUTHOR, COURSE_NAME)
+values(100002, 'OM Books', 'learn aws lambda');
+
+insert into COURSE(ID, AUTHOR, COURSE_NAME)
+values(100003, 'Rupa', 'learn kotlin');
